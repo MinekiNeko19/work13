cda308e5acb98054ba5b9ab3b98128519518ddc7
Sarah Leong <sleong20@stuy.edu>
2021-11-07 21:24:52 -0500

    fixed the seg fault in changing the borough

--- nyc.c
+++ nyc.c
@@ -5,7 +5,7 @@ int main(int argc, char *argv[]) {
     char line[100];
 
   strcpy(instruct, argv[1]);
-  printf("received instructions: %s\n", instruct);
+  // printf("received instructions: %s\n", instruct);
 
     if (argv[1]) {
       if (!strcmp(instruct, "-read_csv")) {
@@ -16,9 +16,9 @@ int main(int argc, char *argv[]) {
       }
       else if (!strcmp(instruct, "-add_data")) {
         add_data();
-      // }
-      // else if (!strcmp(instruct, "-updata_data")) {
-      //   updata_data();
+      }
+      else if (!strcmp(instruct, "-update_data")) {
+        update_data();
       } else {
         printf("%s is an invalid action call\n", argv[1]);
       }
@@ -114,6 +114,7 @@ int read_csv() {
   close(file);
   return errno;
 }
+
 void read_data() {
   printf("\nread_data called\n");
   struct stat sb;
@@ -131,6 +132,7 @@ void read_data() {
   }
   close(ogcopy);
 }
+
 void add_data() {
   printf("\nadd_data called\n");
 
@@ -139,24 +141,57 @@ void add_data() {
   struct pop_entry d;
 
   char input[100];
-  printf("Enter the year: ");
+  printf("Enter the year boro pop: ");
   fgets(input, sizeof(input), stdin);
-  sscanf(input,"%d",&d.year);
-
-
-  printf("Enter the borough: ");
-  fgets(input, sizeof(input), stdin);
-  sscanf(input,"%s",d.boro);
-  
-  printf("Enter the population: ");
-  fgets(input, sizeof(input), stdin);
-  sscanf(input,"%d",&d.population);
+  sscanf(input,"%d %s %d",&d.year,d.boro,&d.population);
 
   // printf("year: %d\tpopulation: %d\tboro: %s\n",d.year,d.population,d.boro);
   write(file,&d,sizeof(struct pop_entry));
 
   close(file);
 }
-// void updata_data() {
-//   printf("updata_data called\n");
-// }
+
+void update_data() {
+  printf("update_data called\n");
+
+  struct stat sb;
+  stat("nyc_pop.data", &sb);
+
+  struct pop_entry d[sb.st_size/sizeof(struct pop_entry)];
+  printf("number of entries: %lu\n", sb.st_size/sizeof(struct pop_entry));
+  int ogcopy = open("nyc_pop.data", O_RDONLY, 0);
+  read(ogcopy,d,sb.st_size);
+  // printf("year: %d, pop: %d, boro: %s\n", d[0].year, d[0].population, d[0].boro);
+  int i = 0;
+  while (i < sb.st_size/sizeof(struct pop_entry)) {
+    printf("%d:\tyear: %d\t pop: %d\t boro: %s\n", i, d[i].year, d[i].population, d[i].boro);
+    i++;
+  }
+  close(ogcopy);
+
+  ogcopy = open("nyc_pop.data", O_WRONLY, 0);
+  char buffer[100];
+  int entry;
+  printf("entry to update: ");
+  fgets(buffer, sizeof(buffer), stdin);
+  sscanf(buffer,"%d",&entry);
+
+  struct pop_entry fixit;
+  printf("Enter year boro pop: ");
+  fgets(buffer, sizeof(buffer), stdin);
+  sscanf(buffer, "%d %s %d", &fixit.year, fixit.boro, &fixit.population);
+  
+  printf("%d:\tyear: %d\t pop: %d\t boro: %s\n", entry, d[entry].year, d[entry].population, d[entry].boro);
+  d[entry].year = fixit.year;
+  d[entry].population = fixit.population;
+  strncpy(d[entry].boro,fixit.boro,15);
+  printf("%d:\tyear: %d\t pop: %d\t boro: %s\n", entry, d[entry].year, d[entry].population, d[entry].boro);
+
+  int j = 0;
+  while(j < sb.st_size/sizeof(struct pop_entry)) {
+    write(ogcopy,&d[j],sizeof(struct pop_entry));
+    j++;
+  }
+
+  close(ogcopy);
+}
